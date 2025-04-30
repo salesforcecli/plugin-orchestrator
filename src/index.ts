ffc4e879af98fdf5b281b3406dda80a908f2335d
ricardo-viera <ricardo.viera@salesforce.com>
2025-04-30 09:51:17 -0600

    fix: simplify project by removing schemas and primitive types

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,4 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-// Import primitive types
-import './primitive-types.js';
-
-// These named exports are required for the OCLIF schema generator
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const string: any = String;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const number: any = Number;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const boolean: any = Boolean;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const object: any = Object;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const array: any = Array;
-
-// Main export
 export default {};
